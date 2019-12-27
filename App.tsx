dc6c7cce52921581489ff0d6bd3997e232cc4816
blocksan <sandyghosh555@gmail.com>
2019-12-28 01:19:43 +0530

    feat: kitten library added

--- App.tsx
+++ App.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StyleSheet, View } from 'react-native';
+import { ApplicationProvider, Layout,Button, Text } from '@ui-kitten/components';
+import { mapping, light as lightTheme } from '@eva-design/eva';
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open App.tsx to start working on your app!</Text>
-    </View>
+      <ApplicationProvider mapping={mapping} theme={lightTheme}>
+      <Layout style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+        <Text category='h1'>HOME</Text>
+        <Button style={{borderRadius:50}} status='primary'>PRIMARY</Button>
+      </Layout>
+      </ApplicationProvider>
   );
 }
 
